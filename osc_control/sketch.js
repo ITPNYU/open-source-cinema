4a1ea478fb513b2dab5981994dc5d80a7b9f7254
Dano <dan.osullivan@nyu.edu>
2017-04-17 11:18:30 -0400

    not tracking all bodies

--- osc_control/sketch.js
+++ osc_control/sketch.js
@@ -1,103 +1,16 @@
 var objects = [];
 var selectedObject;
-var cube;
+var handProxy;
 
 // Three.js variables
 var width = window.innerWidth;
 var height = window.innerHeight;
 var camera3D;  //be careful because p5.js might have something named camera
-var scene;  //be careful because our sketch has something named scene
+var scene;
 var renderer;
 
-
 var kinectron = null;
-var kinectronIpAddress = "172.16.223.176"; // FILL IN YOUR KINECTRON IP ADDRESS HERE
 var mode = "keying";
-
-
-//key pressed won't work unless you wake up p5 but we are using three.js's "init" funciton instead
-function setup(){
-}
-
-function keyPressed(){
-  console.log(keyCode);
-  if(selectedObject){
-    if (keyCode == 37){
-      selectedObject.rotation.y =   selectedObject.rotation.y  + Math.PI/18;
-      saveKeying(selectedObject);
-    }else if (keyCode == 39){
-      selectedObject.rotation.y =   selectedObject.rotation.y  - Math.PI/18;
-      saveKeying(selectedObject);
-    }else if (keyCode == 38){
-      selectedObject.position.z =   selectedObject.position.z  + 10;
-      saveKeying(selectedObject);
-    }else if (keyCode == 40){
-      selectedObject.position.z =   selectedObject.position.z - 10;
-      saveKeying(selectedObject);
-    }
-
-  }
-
-  //change modes
-  if (key == ' '){ //space key
-
-    if (mode == "keying"){
-      kinectron.stopAll();
-      kinectron.startBodies(gotBody);
-      mode = "moving"
-      console.log("change to moving");
-    }else{
-      kinectron.stopAll();
-      kinectron.startKey(gotKey);
-      mode = "keying"
-      console.log("change to keying");
-    }
-  }
-
-}
-
-function gotBody(data){
-
-  var whichBody = -1;
-  //if multiple bodies come in find the first one with some joints.
-  for(var i = 0; i < data.bodies.length; i++){
-    if(data.bodies[i].joints){
-      whichBody = i;
-      break;
-    }
-  }
-  //if none have joints skip
-  if (whichBody == -1) return;
-  //numbers come in normalized 0-1 and you need to scale them to your space
-  var scaleBy = 500;
-  var leftHand = data.bodies[whichBody].joints[kinectron.HANDLEFT];
-  var rightHand = data.bodies[whichBody].joints[kinectron.HANDRIGHT];
-  x = map(leftHand.cameraX,0,1,0,scaleBy);
-  y = map(leftHand.cameraY,0,1,0,scaleBy);
-  z = map(leftHand.cameraZ,0,1,scaleBy,0);
-  cube.position.set(x,y,-z);
-  //go look what the cube is intersectin with
-  var closestObject = checkIntersections();
-  if (closestObject[0] != -1){
-    //if they made a fist gesture
-    if(data.bodies[whichBody].leftHandState == 3){
-      //console.log(closestObject);
-      var closestObjectIndex = closestObject[0];
-      selectedObject = objects[closestObjectIndex];
-      //console.log(selectedObject);
-      var handPosition = new THREE.Vector3();
-      handPosition.setFromMatrixPosition( cube.matrixWorld );
-
-      var handPosInWorld = handPosition; //camera3D.localToWorld( handPosition  );
-      console.log(handPosInWorld);
-      selectedObject.position.set(handPosInWorld.x, handPosInWorld.y, handPosInWorld.z);
-      selectedObject.rotation.set(rightHand.orientationX, rightHand.orientationY, rightHand.orientationZ);
-    }
-  }
-  //  console.log(data.bodies[0]);
-
-
-}
 
 //Three.js version of p5's "setup"
 function init() {
@@ -117,43 +30,143 @@ function init() {
   } );
   var mesh = new THREE.Mesh( geometry, material );
   scene.add( mesh );
-
-
-  //video
-  //  var vidObject = addVideo("sintel.mp4");
-  //  scene.add(vidObject);
-  //  objects.push(vidObject);
-  htmlInterface();
-
   ///UI
-  document.addEventListener( 'mousedown', onDocumentMouseDownCheckObject, false); //check for clicks
+  htmlInterface();
+  //document.addEventListener( 'mousedown', onDocumentMouseDownCheckObject, false); //check for clicks
   activatePanoControl(camera3D); //field mouse dragggin to move camera
 
+//UGLY LITTLE PROXY FOR YOUR handPosition
   var geometry = new THREE.BoxGeometry( 1, 1, 1 );
   var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-  cube = new THREE.Mesh( geometry, material );
-  //scene.add(cube);
+  handProxy = new THREE.Mesh( geometry, material );
+  //scene.add(handProxy);
   scene.add(camera3D);//add the camera to the scene
-  camera3D.add( cube ); // then add the cube to the camera
-  cube.position.set(0,0,-10);
+  camera3D.add( handProxy ); // then add the handProxy to the camera so it follow it
+  handProxy.position.set(0,0,-10);
+}
 
+//key pressed won't work unless you wake up p5 but we are using three.js's "init" funciton instead
+function setup(){
+}
+function htmlInterface(){
+  //moved most of these out into html file instead of creating them in p5js
+  name_field = $("#name");
+  name_field.val("Dan");
+  scene_field = $("#sceneNum");
+  plot_point_field = $("#plot_point");
+  $("#previous").click(previous);
+  $("#next").click(next);
+  $("#previous_plot_point").click(previousPlotPoint);
+  $("#next_plot_point").click(nextPlotPoint);
+  scene_field.val(sceneNum);
+  plot_point_field.val(plotPoints[plotPoint]);
+  //if they hit return in ip address text entry field than connect to kinectron
+  $("#kinectron_address").on('keyup', function (e) {
+    if (e.keyCode == 13) {
+      connectToKinectron($("#kinectron_address").val());
+    }
+  });
+}
+
+function connectToKinectron(ipAddress){
+  //if you already have a connection kill it
+  if(kinectron) kinectron.stopAll();
   // Define and create an instance of kinectron
-  kinectron = new Kinectron(kinectronIpAddress);
-  //kinectron = new Kinectron("kinectron.itp.tsoa.nyu.edu");  //for one at itp
+  kinectron = new Kinectron(ipAddress);
   // Connect remote to application
   kinectron.makeConnection();
-  //key
-  var keyObject = setupKey(); //connect to kinectron an get a keyed image
-  scene.add(keyObject );
+  mode = "keying"
+  //this is using object oriented coding for the a kinectronKey
+  var keyObject = new kinectronKey(-200,0,300);
+//plane.position.z = 300;
+//  var keyObject = new kinectronKey(handProxy.position.x, handProxy.position.y, handProxy.position.z); //connect to kinectron an get a keyed image
+  scene.add(keyObject.getMesh() );
+  selectedObject = keyObject;
   objects.push(keyObject );  //put it in the list of things that you check for mouse clicks on
+  kinectron.startKey(gotKey);//listen to the kinectron with this object
+  console.log("Connected to " + ipAddress );
+//  console.log(keyObject);
+}
 
+function gotKey(data){
+//relay the general callback to the particular key object
+   selectedObject.gotKey(data);
+}
+
+function keyPressed(){
+
+// moving things the old way of moving things around
+  if(selectedObject){
+    var thisMesh = selectedObject.getMesh();
+    console.log(thisMesh);
+    if (keyCode == 37){
+      thisMesh.rotation.y =   thisMesh.rotation.y  + Math.PI/18;
+      saveKeying(selectedObject);
+    }else if (keyCode == 39){
+      thisMesh.rotation.y =   thisMesh.rotation.y  - Math.PI/18;
+      saveKeying(selectedObject);
+    }else if (keyCode == 38){
+      thisMesh.position.z =   thisMesh.position.z  + 10;
+      saveKeying(selectedObject);
+    }else if (keyCode == 40){
+      thisMesh.position.z =   thisMesh.position.z - 10;
+      saveKeying(selectedObject);
+    }
+  }
+  //space key change modes
+  if (key == ' '){ //space key
+    if (mode == "keying"){
+      kinectron.stopAll();
+      kinectron.startTrackedBodies(gotBody);
+      mode = "moving"
+      console.log("change to moving");
+    }else{
+      kinectron.stopAll();
+      kinectron.startKey(gotKey);
+      mode = "keying"
+      console.log("change to keying");
+    }
+  }
+
+}
+
+function gotBody(data){
+  //numbers come in normalized 0-1 and you need to scale them to your space
+  var scaleBy = 500;
+  var leftHand = data.joints[kinectron.HANDLEFT];
+  var rightHand = data.joints[kinectron.HANDRIGHT];
+  x = map(leftHand.cameraX,0,1,0,scaleBy);
+  y = map(leftHand.cameraY,0,1,0,scaleBy);
+  z = map(leftHand.cameraZ,0,1,scaleBy,0);
+  handProxy.position.set(x,y,-z);
+  //go look what the handProxy is intersectin with
+  var closestObject = checkIntersections();
+  if (closestObject[0] != -1){
+    //if they made a fist gesture
+    if(data.leftHandState == 3){
+
+      var closestObjectIndex = closestObject[0];
+      selectedObject = objects[closestObjectIndex];
+      //find the position of the handProxy and pretend it is your hand.
+      var handPosition = new THREE.Vector3();
+      handPosition.setFromMatrixPosition( handProxy.matrixWorld );
+      var handPosInWorld = handPosition; //camera3D.localToWorld( handPosition  );
+      //console.log(handPosInWorld);
+      selectedObject.getMesh().position.set(handPosInWorld.x, handPosInWorld.y, handPosInWorld.z);
+      //this only works so so, might consider using the rotation of another joint
+      //selectedObject.getMesh().rotation.set(rightHand.orientationX, rightHand.orientationY, rightHand.orientationZ);
+    }
+  }
+  //  console.log(data);
 }
 
 //Three.js version of p5's "draw"
 function animate() {
   requestAnimationFrame(animate);
-  // Update the textures for each animate frame
-  animateKey();
+  for(var i = 0; i < objects.length; i++){
+    objects[i].animate();
+  }
+
   // animateVideo();
   renderer.render(scene,   camera3D);
 }
@@ -187,9 +200,9 @@ function checkIntersections(){
   scene.updateMatrixWorld(true);
   for (var i = 0; i < objects.length; i++){
     var handPosition = new THREE.Vector3();
-    handPosition.setFromMatrixPosition( cube.matrixWorld );
+    handPosition.setFromMatrixPosition( handProxy.matrixWorld );
     var objectPosition = new THREE.Vector3();
-    objectPosition.setFromMatrixPosition( objects[i].matrixWorld );
+    objectPosition.setFromMatrixPosition( objects[i].getMesh().matrixWorld );
     var distance = handPosition.distanceTo(objectPosition);
     if (distance < shortestDistance){
       shortestDistance = distance;
@@ -199,3 +212,8 @@ function checkIntersections(){
   var winnings = [winningObject,shortestDistance];
   return winnings;
 }
+
+//video
+//  var vidObject = addVideo("sintel.mp4");
+//  scene.add(vidObject);
+//  objects.push(vidObject);
