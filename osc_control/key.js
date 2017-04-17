042e1a6dc53cec7f552ee931491452a05768c6af
Dano <dan.osullivan@nyu.edu>
2017-04-17 11:15:59 -0400

    made this object oriented

--- osc_control/key.js
+++ osc_control/key.js
@@ -1,54 +1,53 @@
-// Declare kinectron
-var kinectron = null;
-var kinectronIpAddress = "172.16.220.236"; // FILL IN YOUR KINECTRON IP ADDRESS HERE
-// Use two canvases to draw incoming feeds
-var canvas;
-var ctx;
-var green_screen_texture;
 
-// set a fixed 2:1 for the images
-var CANVW = 512;
-var CANVH = 512;
+function kinectronKey(x,y,z){// Use two canvases to draw incoming feeds
 
-function setupKey(){
-      kinectron.startKey(gotKey);
   // Setup canvas and context
-  canvas = document.getElementById('canvas1');
-  canvas.width = CANVW;
-  canvas.height = CANVH;
-  ctx = canvas.getContext('2d');
+  //  this.canvas = document.createElement('canvas');
+  //  this.canvas = $('<canvas/>',{'class':'kinectScratchPad'}).width(512).height(512);
+  this.canvas = document.getElementById('canvas1');
 
-  var geo = new THREE.PlaneGeometry(100,100);
-  green_screen_texture = new THREE.Texture(canvas);  //make this global because you have to update it
-  //var mat = new THREE.MeshBasicMaterial({ map: green_screen_texture });
+ this.canvas.width = 512;
+  this.canvas.height = 512;
+//  this.canvas.display = "none";
+  //this.canvas.visible = false;
 
-  var mat = new THREE.MeshBasicMaterial({ map: green_screen_texture , transparent: true, opacity: 1, side: THREE.DoubleSide });
-  var plane = new THREE.Mesh(geo, mat);
-  plane.position.y = 0;
-  plane.position.x = -200;
-  plane.position.z = 300;
-  plane.scale.set(4,4,1);
-  return plane;
+  this.ctx = this.canvas.getContext('2d');
 
-}
+  this.geo = new THREE.PlaneGeometry(100,100);
+  this.green_screen_texture = new THREE.Texture(this.canvas);  //make this global because you have to update it
 
-function gotKey(data) {
-  // Image data needs to be draw to img element before canvas
-  //  console.log(data);
-  var img1 = new Image;
-  ctx.clearRect(0,0, CANVW, CANVH);
-  ctx.drawImage(data,0,0, CANVW, CANVH);
+  this.mat = new THREE.MeshBasicMaterial({ map: this.green_screen_texture , transparent: true, opacity: 1, side: THREE.DoubleSide });
+  //this.mat = new THREE.MeshBasicMaterial( { color: 0xff0000 } );
+  this.plane = new THREE.Mesh(this.geo, this.mat);
+  this.plane.position.x = x;
+  this.plane.position.y = y;
+  this.plane.position.z = x;
+  this.plane.scale.set(4,4,1);
 
-}
+  this.animate = function(){
+    // Update the textures for each animate frame
+    this.green_screen_texture.needsUpdate = true;
+  };
 
-function animateKey(){
-  green_screen_texture.needsUpdate = true;
-}
+  this.getMesh = function(){
+    return this.plane;
 
-function startRecord(){
-  kinectron.startRecord();
-}
+  };
 
-function stopRecord(){
-  kinectron.stopRecord();
-}
+  this.gotKey = function(data){
+    //this is called when kinectron has a keyed image to display
+    // Image data needs to be draw to img element before canvas
+    //might have to add this style in some chrome browsers
+    /*var newImg = new Image();
+    newImg.src = data; // image data from kinectron
+
+    newImg.onload = function () { //needs onload function
+      this.ctx.clearRect(0, 0, hiddenContext.canvas.width, hiddenContext.canvas.height);
+      this.ctx.drawImage(newImg, 0, 0);
+    }*/
+     this.ctx.clearRect(0,0, 512, 512);
+    this.ctx.drawImage(data, 0,0, 512, 512);
+  };
+
+
+};
