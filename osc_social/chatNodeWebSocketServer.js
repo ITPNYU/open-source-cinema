1886486686e9b3ce96dbdaf09be7793274d74e42
Dano <dan.osullivan@nyu.edu>
2019-05-03 13:26:02 -0400

    updated BodyPix

--- osc_social/chatNodeWebSocketServer.js
+++ osc_social/chatNodeWebSocketServer.js
@@ -10,6 +10,7 @@
 // Port where we'll run the websocket server
 var webSocketsServerPort = 1337;
 
+
 // websocket and http servers
 var webSocketServer = require('websocket').server;
 var http = require('http');
@@ -21,7 +22,7 @@ var http = require('http');
 var history = [];
 // list of currently connected clients (users)
 var clients = [];
-
+var position = 0;
 
 /**
  * HTTP server
@@ -38,6 +39,7 @@ server.listen(webSocketsServerPort, function () {
  */
 // WebSocket server is tied to a HTTP server. WebSocket request is just
 // an enhanced HTTP request. For more info http://tools.ietf.org/html/rfc6455#page-6
+//var wsServer = new webSocketServer({ httpServer: server ,'grpc.max_send_message_length': 1024*1024*1024*100});
 var wsServer = new webSocketServer({ httpServer: server });
 
 // This callback function is called every time someone
@@ -53,37 +55,69 @@ wsServer.on('request', function (request) {
     var index = clients.push(connection) - 1;
     var ID = false;
 
+
     console.log((new Date()) + ' Connection accepted.');
 
     //use the time as user name (hack) and send it back to them
     var ID = (new Date()).getTime();
-    connection.send(JSON.stringify({ type: 'initial', contents:{ ID: ID} }));
+
+    //place them across from eachother somewhere on a circle
+    position += 180;
+    if (clients.length % 2 == 0) position += 180 / clients.length
+    let p = position % 360
+
+    //upon first connecting, assign them an ID and a place
+    connection.send(JSON.stringify({ type: 'initial', contents: { "ID": ID, "position": p, "utf8Data": "{}" } }));
+
+
 
     // send back chat history
-    if (history.length > 0) {
-        connection.send(JSON.stringify({ type: 'history', contents: history }));
-    }
+    // if (history.length > 0) {
+    //     connection.send(JSON.stringify({ type: 'history', contents: history }));
+    //  }
 
     // user sent some message
     connection.on('message', function (message) {
-       // if (message.type === 'utf8') { // accept only text
-            console.log((new Date()) + ' Received Message from '+ ID + ': ' + message.utf8Data);
-           // if (message.utf8Data.text != "")
-            history.push(message);
-            history = history.slice(-100);
-            // broadcast message to all connected clients
-            var json = JSON.stringify({ type: 'message', contents: message });
+
+        let thisJSON = JSON.parse(message.utf8Data);
+        let type = thisJSON.type;
+        if (type == "pic") {
+            var json = JSON.stringify({ type: 'pic', contents: message });
             for (var i = 0; i < clients.length; i++) {
                 clients[i].send(json);
             }
+        } else if (type == "text") {
+            var json = JSON.stringify({ type: 'text', contents: message });
+            for (var i = 0; i < clients.length; i++) {
+                clients[i].send(json);
+            }
+            history.push(message);
+            history = history.slice(-100);
+        }
+        // else if (type == "position") {
+        //     var json = JSON.stringify({ type: 'position', contents: message });
+        //     for (var i = 0; i < clients.length; i++) {
+        //         clients[i].send(json);
+        //     }
+        // }
+        // if (message.type === 'utf8') { // accept only text
+        //console.log((new Date()) + ' Received Message from '+ ID + ': ' + message.utf8Data);
+        // if (message.utf8Data.text != "")
+        //  history.push(message);
+        //  history = history.slice(-100);
+        // broadcast message to all connected clients
+        //  var json = JSON.stringify({ type: 'message', contents: message });
+        // for (var i = 0; i < clients.length; i++) {
+        //   clients[i].send(json);
+        // }
         //}
     });
 
-    
+
     // user disconnected
     connection.on('close', function (connection) {
-        if (ID !== false ) {
-            console.log((new Date()) + " Peer " + connection.remoteAddress + " disconnected.");
+        if (ID !== false) {
+            console.log((new Date()) + " Peer " + connection.remoteAddress + " disconnected." + ID + " " + index);
             // remove user from the list of connected clients
             clients.splice(index, 1);
         }
