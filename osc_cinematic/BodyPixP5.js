7d5d7a27491cc18b707aa2445457501916b761db
Dano <dan.osullivan@nyu.edu>
2020-04-01 15:12:14 -0400

    new for 2020

---
+++ osc_cinematic/BodyPixP5.js
@@ -0,0 +1,41 @@
+let bodypix;
+let video;
+let segmentation;
+let img;
+
+const options = {
+  outputStride: 8, // 8, 16, or 32, default is 16
+  segmentationThreshold: 0.3, // 0 - 1, defaults to 0.5 
+}
+
+let body_pix_p5_canvas;
+
+function setup() {
+  body_pix_p5_canvas = createCanvas(256, 256);
+  // load up your video
+  video = createCapture(VIDEO);
+  video.size(160, 120);
+  video.hide(); // Hide the video element, and just show the canvas
+  bodypix = ml5.bodyPix(video, modelReady);
+  loadP5Sketch(body_pix_p5_canvas.elt,0,0,200); 
+  body_pix_p5_canvas.style('display', 'none');// hide this because I want to use in three.js
+  console.log("setup body  pix"); 
+}
+
+function modelReady() {
+  console.log('body pix model ready!')
+  bodypix.segmentWithParts(gotResults, options)
+}
+
+function gotResults(err, result) {
+ // console.log(result);
+  if (err) {
+    console.log("Oh My" + err)
+    return
+  }
+  segmentation = result;
+  clear();
+  image(segmentation.personMask, 0, 0);
+
+bodypix.segmentWithParts(gotResults, options);
+}
---
+++ osc_cinematic/TalkToRunwayP5.js
@@ -0,0 +1,30 @@
+//var runway_img;
+let runway_canvas;
+function setup() {
+  runway_canvas = createCanvas(512, 512);
+//  runway_img = createImage(width,height);
+  loadP5Sketch(runway_canvas.elt,0,0,200); 
+  runway_canvas.style('display', 'none');// hide this because I want to use in three.js
+  console.log("setup runway"); 
+}
+
+function talkToRunway(query) {
+  const path = 'http://localhost:8000/query';
+  console.log("askit");
+  const data = {
+    "caption": query
+  };
+  httpPost(path, 'json', data, gotImage, gotError);
+}
+
+function gotError(error) {
+  console.error(  error);
+}
+
+function gotImage(data) {
+  console.log("Got Image Data" + data.length);
+  letrunway_img= createImg(data.result,"image generated in runway");
+  runway_img.hide();
+  clear();
+  image(runway_img, 0, 0);
+}
