d33d8dd9f6745820193e99abf6c4b8a742505382
Dano <dan.osullivan@nyu.edu>
2020-04-10 12:37:08 -0400

    simpler version

--- body20/PoseNetP5Instance.js
+++ body20/PoseNetP5Instance.js
@@ -12,27 +12,29 @@ const poseNetSketch = function(p) {
 let video;
 let poseNet;
 let poses = [];
-let angle = 0;
+let thisCanvas ;
 
 p.setup = function() {
-p.createCanvas(640, 480);
+  poseNetCanvas = p.createCanvas(640, 480);
 
-///ONLY DO THIS PART IF YOU WANT TO USE A USB CAMERA
+///ONLY DO THIS PART IF YOU WANT TO USE A SEPARATE USB CAMERA
   //navigator.mediaDevices.enumerateDevices().then(function(d){console.log(d);});
   //un comment above look in the console for the video input device you are after
+  /*
    var options = {
      video: {
        optional: [{
           sourceId: '5289ce1802c3a8580144fdb57108c6c5866e9095eed6024105418a989e4e074e'
       }]
      }
-  };
+  };*/
+ //video = p.createCapture( options);
 
-  video = p.createCapture( options);
-  // load up your video
   //OTHERWISE LOAD UP DEFAULT CAMERA
-  //video = p.createCapture(p.VIDEO);
+  video = p.createCapture(p.VIDEO);
   video.size(p.width, p.height);
+
+  //load up posenet
   let  params = {
     architecture: 'MobileNetV1',
     imageScaleFactor: 0.3,
@@ -42,32 +44,32 @@ p.createCanvas(640, 480);
     maxPoseDetections: 5,
     scoreThreshold: 0.5,
     nmsRadius: 20,
-    detectionType: 'multiple',
+    detectionType: 'single',
     inputResolution: 513,
     multiplier: 0.75,
     quantBytes: 2,
   };
   // Create a new poseNet method with a single detection
   poseNet = ml5.poseNet(video,params, modelReady);
-
   // Hide the video element, and just show the canvas
   video.hide();
-  p.imageMode(p.CENTER);
+  poseNetCanvas.style('display', 'none');// hide this because I want to use in three.js
 };
 
 p.getPoses = function(){
     return poses;
 };
 
+
 function modelReady() {
   poseNet.on('pose', gotPose);
+  console.log("model ready");
   // This sets up an event that fills the global variable "poses"
 
 }
 
 function gotPose(results) {
   poses = results;
-  // console.log(degrees(angle));
 }
 
 
